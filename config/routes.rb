4f41058b6b94f8a6bd38391bebec72a211e6f121
suresh subramani <suresh@luxire.com>
2018-12-10 11:34:36 +0530

    5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get 'hello_world/hello'
+
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
